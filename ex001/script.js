30802ad710f234d8df188528a25aaf6586ab5a7a
caio.amaro <caioamarocci@gmail.com>
2025-06-26 11:24:28 -0300

    adiciona exercicio

--- ex001/script.js
+++ ex001/script.js
@@ -1,6 +1,24 @@
-console.log(`hello world`);
+console.log('hello world');
 let x = 5;
 let y = 10;
-let result = x + y;
-console.log(`x = ${x}` + `, y = ${y}` + `, result = ${result}`);
+let soma = x + y;
+let multiplicacao = x * y;
 
+console.log('A soma de ${x} e ${y} é ${soma}');
+console.log('A multiplicação de ${x} e ${y} é ${multiplicacao}');
+
+alert('hello world'); 
+
+let campo = prompt('Digite seu nome:');
+let confirmar = confirm('você deseja excluir usuario?');
+
+if(confirmar) {
+    alert('Usuário excluído: ' + campo);
+}
+else { 
+    alert('Usuário não excluído');
+}
+
+
+console.log('Seu nome é: ' + campo);
+console.log('Confirmação: ' + confirmar);
